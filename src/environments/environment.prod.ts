6f2c7a11705b0f5a7ba8edbc19ec94e4591aa0e2
Kiran Panigrahi <panigrahi.kiran@gmail.com>
2019-04-06 21:48:32 -0700

    registered fake url in environments file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  appName: 'automigrate'
+  appName: 'automigrate',
+  apiURL: 'http://localhost:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'automigrate'
+  appName: 'automigrate',
+  apiURL: 'http://localhost:3000'
 };
 
 /*
